fbf80e7a28a268c18d2993b36371d1e00d2355e4
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-08-12 15:56:03 +0500

    Initial Commit

---
+++ AnimationApp/AnimationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationAppApp.swift
+//  AnimationApp
+//
+//  Created by A'zamjon Abdumuxtorov on 12/08/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
