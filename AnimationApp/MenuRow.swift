39ba028a2c959d4630c9d132830896f249de6312
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-08-14 12:37:22 +0500

    UI animations update

---
+++ AnimationApp/MenuRow.swift
@@ -0,0 +1,18 @@
+//
+//  MenuRow.swift
+//  AnimationApp
+//
+//  Created by A'zamjon Abdumuxtorov on 14/08/24.
+//
+
+import SwiftUI
+
+struct MenuRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuRow()
+}
